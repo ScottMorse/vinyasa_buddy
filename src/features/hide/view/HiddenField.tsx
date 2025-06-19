32366ddf80db07499c3f8bbe99ff9357db5c0508
ScottMorse <scottmorsedev@gmail.com>
2025-06-18 23:20:17 -0600

    Test

--- src/features/hide/view/HiddenField.tsx
+++ src/features/hide/view/HiddenField.tsx
@@ -1,3 +1,4 @@
+import { useInterval } from '@/utils/hooks';
 import { Box, Stack, Text } from '@chakra-ui/react';
 import { type Scope, animate, createScope } from 'animejs';
 import { useEffect, useRef, type ReactNode } from 'react';
@@ -16,6 +17,8 @@ export const HiddenField = ({
   const { current: initialIsHidden } = useRef(isHidden);
 
   const root = useRef<HTMLDivElement>(null);
+  const hiderRef = useRef<HTMLDivElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const scope = useRef<Scope>(null);
 
   useEffect(() => {
@@ -27,27 +30,40 @@ export const HiddenField = ({
     });
   }, [isHidden]);
 
+  useInterval({
+    duration: 100,
+    callback: () => {
+      if (hiderRef.current && wrapperRef.current) {
+        hiderRef.current.style.width = `${wrapperRef.current.scrollWidth}px`;
+        hiderRef.current.style.height = `${wrapperRef.current.scrollHeight}px`;
+      }
+    },
+    enabled: true,
+  });
+
   return (
-    <Box
+    <Stack
       width="fit-content"
       height="fit-content"
       position="relative"
+      align="center"
+      justify="center"
       ref={root}
     >
       <Stack
         className="hidden-field"
         position="absolute"
         pointerEvents="none"
-        top="0"
-        left="0"
+        top="-0.5rem"
         width="100%"
         height="100%"
         bg="backgroundSecondary"
         borderRadius="xl"
-        p="0.25rem"
+        p="0.5rem"
         boxSizing="content-box"
         align="center"
         justify="center"
+        ref={hiderRef}
         opacity={initialIsHidden ? 1 : 0}
       >
         <Text
@@ -55,11 +71,14 @@ export const HiddenField = ({
           color="textPrimary"
           opacity={0.65}
           whiteSpace="nowrap"
+          m="0.5rem"
         >
           {fieldName} Hidden
         </Text>
       </Stack>
-      {children}
-    </Box>
+      <Box width="fit-content" height="fit-content" ref={wrapperRef}>
+        {children}
+      </Box>
+    </Stack>
   );
 };
