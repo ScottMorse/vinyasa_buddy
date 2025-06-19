0f6b1bb9cb79965f18c533ff6c3f4a154be8fe34
ScottMorse <scottmorsedev@gmail.com>
2025-06-18 21:33:51 -0600

    Prepare to test gh-pages build & deploy

--- rspack.config.ts
+++ rspack.config.ts
@@ -9,6 +9,8 @@ const STATIC_DIR = `${__dirname}/public`;
 // Target browsers, see: https://github.com/browserslist/browserslist
 const targets = ['last 2 versions', '> 0.2%', 'not dead', 'Firefox ESR'];
 
+const PUBLIC_PATH = process.env.PUBLIC_PATH || '/';
+
 export default defineConfig({
   devServer: {
     port: 3000,
@@ -16,6 +18,12 @@ export default defineConfig({
     static: {
       directory: STATIC_DIR,
     },
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+    path: `${__dirname}/build`,
+    publicPath: PUBLIC_PATH,
   },
   entry: {
     main: './src/main.tsx',
@@ -58,7 +66,36 @@ export default defineConfig({
   },
   plugins: [
     new rspack.HtmlRspackPlugin({
-      template: './index.html',
+      templateContent: `<!doctype html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+    <meta http-equiv="Cache-Control" content="no-cache" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    ${PUBLIC_PATH === '/' ? '' : `<base href="${PUBLIC_PATH}" />`}
+		<link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Vinyasa Buddy</title>
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Monoton&family=Rubik:wght@300..700&display=swap"
+			rel="stylesheet"
+		/>
+    <style>
+      :root {
+        --font-heading: 'Monoton', sans-serif;
+        --font-default: 'Rubik', sans-serif;
+      }
+    </style>
+
+	</head>
+	<body>
+		<div id="root"></div>
+	</body>
+</html>
+`,
     }),
     isDev ? new ReactRefreshRspackPlugin() : null,
     new rspack.CopyRspackPlugin({
